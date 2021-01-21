c48e9ffb72982118cbff85012b4b365cfc86062a
“zed” <zed@kolk.me>
2021-01-21 15:50:44 -0600

    commit tree 99db70f2e7791f29ddf24cadc2b068b0e7128e23

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+require_relative "../lib/user.rb"
+class Student < User
+    attr_accessor :knowledge
+    def initialize
+        super
+        @knowledge = []
+    end
+    def learn knowledge
+        @knowledge << knowledge
+    end
 
 end
